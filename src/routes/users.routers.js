2f1947bfca327974db2e4224f154bf5177fd9004
leejunsung <junstar96@naver.com>
2024-12-02 19:02:01 +0900

    push_check

--- src/routes/users.routers.js
+++ src/routes/users.routers.js
@@ -7,12 +7,33 @@
 
 import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
-
+import user_auth from '../middlewares/auth.middleware.js'
 
 const router = express.Router();
 
-router.post('/gatcha', async (req, res)=>{
+router.post('/gatcha',user_auth, async (req, res) => {
+    try {
+        let playerGatchaInfo = await prisma.$transaction(async (tx)=>{
+            let playerList = await tx.players.findMany({
+
+            });
     
+            let random_count = Math.floor(Math.random() * playerList.length);
+            
+            
+        })
+
+
+        
+
+
+
+    }
+    catch (err) {
+        next(err);
+    }
+
+
 })
 
 
