af68c131519c2b06c8e2319c3d047f9c6c8a35f5
Jaeyoung <jyko1101@gmail.com>
2024-12-02 21:02:17 +0900

    Update matches.routers.js

--- src/routes/matches.routers.js
+++ src/routes/matches.routers.js
@@ -23,116 +23,116 @@ const router = express.Router();
 // validation:
 //====================================================================================================================
 //====================================================================================================================
-// router.post('/matches', authMiddleware, async (req, res, next) => {
-//     // 1. auth로부터 user id가져오기
-//     const { userId } = req.user;
-//     try {
-//         // 2. 랜덤 상대 userId 가져오기
-//         const randomOpponents = await prisma.users.findMany({
-//             where: { id: { not: userId }, }
-//         });
-//         // validation: 랜덤매칭 후보군 없을시
-//         const len = randomOpponents.length;
-//         if (len === 0)
-//             return res.status(404).json({ message: '[Not Found] 매칭상대 찾기 실패.' });
-//         // 랜덤하게 생성
-//         const randomIndex = crypto.randomInt(0, len);
-//         const randomOpponent = randomOpponents[randomIndex];
+router.post('/matches', authMiddleware, async (req, res, next) => {
+    // 1. auth로부터 user id가져오기
+    const { userId } = req.user;
+    try {
+        // 2. 랜덤 상대 userId 가져오기
+        const randomOpponents = await prisma.users.findMany({
+            where: { id: { not: userId }, }
+        });
+        // validation: 랜덤매칭 후보군 없을시
+        const len = randomOpponents.length;
+        if (len === 0)
+            return res.status(404).json({ message: '[Not Found] 매칭상대 찾기 실패.' });
+        // 랜덤하게 생성
+        const randomIndex = crypto.randomInt(0, len);
+        const randomOpponent = randomOpponents[randomIndex];
 
-//         // 2. userId로부터 team > squad 찾기
+        // 2. userId로부터 team > squad 찾기
 
-//         /*
-//         // Legacy code: 현재 상태인 단일 팀만 지원
-//         const userSquad = await prisma.userSquads.findMany({
-//             where: { userTeamId: userId },
-//             include: {
-//                 userteam: {
-//                     include: { players: { include: { playerStats: true }, } }
-//                 },
-//             },
-//         });
-//         const opponentSquad = await prisma.userSquads.findMany({
-//             where: { userTeamId: randomOpponent.id },
-//             include: {
-//                 userteam: {
-//                     include: { players: { include: { playerStats: true }, } }
-//                 },
-//             },
-//         });
-//         */
-//         // renewal: 이후에 유저가 여러개의 팀을 가지더라도 가능하게끔
-//         const userTeams = await prisma.userTeams.findMany({ 
-//             where: { userId } 
-//         });
-//         if (userTeams.length === 0)
-//             return res.status(404).json({ message: '[Not Found] 유저 팀 찾지못함' });
+        /*
+        // Legacy code: 현재 상태인 단일 팀만 지원
+        const userSquad = await prisma.userSquads.findMany({
+            where: { userTeamId: userId },
+            include: {
+                userteam: {
+                    include: { players: { include: { playerStats: true }, } }
+                },
+            },
+        });
+        const opponentSquad = await prisma.userSquads.findMany({
+            where: { userTeamId: randomOpponent.id },
+            include: {
+                userteam: {
+                    include: { players: { include: { playerStats: true }, } }
+                },
+            },
+        });
+        */
+        // renewal: 이후에 유저가 여러개의 팀을 가지더라도 가능하게끔
+        const userTeams = await prisma.userTeams.findMany({ 
+            where: { userId } 
+        });
+        if (userTeams.length === 0)
+            return res.status(404).json({ message: '[Not Found] 유저 팀 찾지못함' });
 
-//         const opponentTeams = await prisma.userTeams.findMany({ 
-//             where: { userId: randomOpponent.id } 
-//         });
-//         if (opponentTeams.length === 0)
-//             return res.status(404).json({ message: '[Not Found] 상대 팀 찾지못함' });
+        const opponentTeams = await prisma.userTeams.findMany({ 
+            where: { userId: randomOpponent.id } 
+        });
+        if (opponentTeams.length === 0)
+            return res.status(404).json({ message: '[Not Found] 상대 팀 찾지못함' });
 
-//         const userSquad = await prisma.userSquads.findMany({
-//             where: { userTeamId: { in: userTeams.map(team => team.id) } },
-//             include: { userteam: { include: { players: { include: { playerStats: true } } } } }
-//         });
-//         if (userSquad.length === 0)
-//             return res.status(404).json({ message: '[Not Found] 유저 스쿼드 찾지못함' });
+        const userSquad = await prisma.userSquads.findMany({
+            where: { userTeamId: { in: userTeams.map(team => team.id) } },
+            include: { userteam: { include: { players: { include: { playerStats: true } } } } }
+        });
+        if (userSquad.length === 0)
+            return res.status(404).json({ message: '[Not Found] 유저 스쿼드 찾지못함' });
 
-//         const opponentSquad = await prisma.userSquads.findMany({
-//             where: { userTeamId: { in: opponentTeams.map(team => team.id) } },
-//             include: { userteam: { include: { players: { include: { playerStats: true } } } } }
-//         });
-//         if (opponentSquad.length === 0)
-//             return res.status(404).json({ message: '[Not Found] 상대 스쿼드 찾지못함' });
+        const opponentSquad = await prisma.userSquads.findMany({
+            where: { userTeamId: { in: opponentTeams.map(team => team.id) } },
+            include: { userteam: { include: { players: { include: { playerStats: true } } } } }
+        });
+        if (opponentSquad.length === 0)
+            return res.status(404).json({ message: '[Not Found] 상대 스쿼드 찾지못함' });
 
-//         // utils에서 게임로직통해 매치에서의 양팀 스코어 생성
-//         const { userSquadScore, opponentSquadScore } = simulateMatch(userSquad, opponentSquad);
-
-
+        // utils에서 게임로직통해 매치에서의 양팀 스코어 생성
+        const { userSquadScore, opponentSquadScore } = simulateMatch(userSquad, opponentSquad);
 
 
-//         // 매치 생성
-//         const match = await prisma.matches.create({
-//             data: {
-//                 matchUserId1: userId,
-//                 matchUserId2: randomOpponent.id,
-//                 matchResult: 'DRAW',
-//             }
-//         });
 
-//         return res.status(200).json({
-//             message: '[Success] match created.',
-//             match,
-//         });
 
-//     } catch (error) {
-//         next(error);
-//     }
-// });
+        // 매치 생성
+        const match = await prisma.matches.create({
+            data: {
+                matchUserId1: userId,
+                matchUserId2: randomOpponent.id,
+                matchResult: 'DRAW',
+            }
+        });
 
-// //====================================================================================================================
-// //====================================================================================================================
-// // 매치메이킹 API: 지정 상대 매치메이킹
-// // URL: /api/matches/:playerId
-// // method: POST
-// // auth: 인증 필요
-// // validation:
-// //====================================================================================================================
-// //====================================================================================================================
-// router.post('/matches/:playerId', authMiddleware, async (req, res, next) => {
-//     // auth로부터 user id가져오기
-//     const { userId } = req.user;
-//     try {
+        return res.status(200).json({
+            message: '[Success] match created.',
+            match,
+        });
 
-//         // 로그인 성공
-//         return res.status(200).json({ message: '[Success] log-in completed.' });
+    } catch (error) {
+        next(error);
+    }
+});
 
-//     } catch (error) {
-//         next(error);
-//     }
-// });
+//====================================================================================================================
+//====================================================================================================================
+// 매치메이킹 API: 지정 상대 매치메이킹
+// URL: /api/matches/:playerId
+// method: POST
+// auth: 인증 필요
+// validation:
+//====================================================================================================================
+//====================================================================================================================
+router.post('/matches/:playerId', authMiddleware, async (req, res, next) => {
+    // auth로부터 user id가져오기
+    const { userId } = req.user;
+    try {
+
+        // 로그인 성공
+        return res.status(200).json({ message: '[Success] log-in completed.' });
+
+    } catch (error) {
+        next(error);
+    }
+});
 
 //====================================================================================================================
 //====================================================================================================================
