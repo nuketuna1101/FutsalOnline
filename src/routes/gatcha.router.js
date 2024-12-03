3aedcc148d01182598db04889b91602ab80f00b2
leejunsung <junstar96@naver.com>
2024-12-03 15:26:53 +0900

    push

--- src/routes/gatcha.router.js
+++ src/routes/gatcha.router.js
@@ -10,48 +10,47 @@ const gatchaPay = 100;
 
 
 //가챠 뽑기 API
-router.post('/gatcha', async (req, res) => {
+router.post('/gatcha', async (req, res, next) => {
     try {
-        const userid = parseInt(req.userid, 10);
-        let [playerName, cash] = await prisma.$transaction(async (tx) => {
-            let playerList = await tx.players.findMany({
+        //const userid = parseInt(req.userid, 10);
+        // let [playerName, cash] = await prisma.$transaction(async (tx) => {
+        //     let playerList = await tx.players.findMany({
 
-            });
+        //     });
 
-            let random_count = Math.floor(Math.random() * playerList.length);
-
-
-            let userteam = await tx.userTeams.create({
-                data: {
-                    userId: {
-                        connect : {
-                            id : 1
-                        }
-                    },
-                    playerId: playerList[random_count].id
-                }
-
-            })
-
-            const updatedUser = await tx.userAccount.update({
-                where: {
-                    id: userid
-                },
-                data: {
-                    cash: {
-                        decrement: gatchaPay
-                    }
-                }
-            })
-
-            return [playerList[random_count].playerName, updatedUser.cash]
-        })
+        //     let random_count = Math.floor(Math.random() * playerList.length);
 
 
+        //     let userteam = await tx.userTeams.create({
+        //         data: {
+        //             userId: {
+        //                 connect : {
+        //                     id : 1
+        //                 }
+        //             },
+        //             playerId: playerList[random_count].id
+        //         }
+
+        //     })
+
+        //     const updatedUser = await tx.userAccount.update({
+        //         where: {
+        //             id: userid
+        //         },
+        //         data: {
+        //             cash: {
+        //                 decrement: gatchaPay
+        //             }
+        //         }
+        //     })
+
+        //     return [playerList[random_count].playerName, updatedUser.cash]
+        // })
 
 
 
-        return res.status(200).json({ Message: ` ${playerName} 를 뽑았습니다. 잔액 ${cash} ` })
+
+        //return res.status(200).json({ Message: ` ${playerName} 를 뽑았습니다. 잔액 ${cash} ` })
     }
     catch (err) {
         next(err);
