e7335f5ec4224798c6e15b9aaeb5fa12659a7aa5
Ju YongChan <tjszhf9616@naver.com>
2024-12-03 19:10:48 +0900

    로그인 끝
    로그인 완료

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import expressSession from 'express-session';
+import expressMySQLSession from 'express-mysql-session';
 // middlewares
 import LoggingMiddleware from './middlewares/logging.middleware.js';
 import ErrorHandlingMiddleware from './middlewares/errorHandling.middleware.js';
@@ -15,12 +17,40 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT_NUMBER;
+const PORT = 3321;
+
+const MySQLStore = expressMySQLSession(expressSession);
+const sessionStore = new MySQLStore({
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
+  expiration: 1000 * 60 * 60 * 24,
+  createDatabaseTable: true,
+});
 
 // Middlewares
 app.use(LoggingMiddleware);
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+    expressSession({
+        secret: process.env.SESSION_SECRET_KEY,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+          // 클라이언트에서 쿠키 접근 차단
+          httpOnly: true, 
+          // HTTPS에서만 작동하도록 설정 (로컬 개발에서는 false)
+          secure: false, 
+          // 1일 동안 쿠키를 사용할 수 있도록 설정한다.
+          maxAge: 1000 * 60 * 60 * 24,
+        },
+        store: sessionStore,
+    })
+);
+
 app.use('/api', [usersRouter, playersRouter, matchesRouter, squadsRouter]);
 app.use(ErrorHandlingMiddleware);
 
--- src/routes/users.routers.js
+++ src/routes/users.routers.js
@@ -22,7 +22,8 @@ router.post('/users/sign-up', async (req, res, next) => {
         });
         // 아이디, 비밀번호 영어소문자 + 숫자 유효성검사용 변수
         function conLowAndNumber(input) {
-            const regex = /^(?=.*[a-z])(?=.*[0-9]).+$/; // 영어 소문자와 숫자가 모두 포함된 문자열
+            // 영어 소문자와 숫자가 모두 포함된 문자열
+            const regex = /^(?=.*[a-z])(?=.*[0-9]).+$/;
             return regex.test(input);
         }
 
@@ -40,29 +41,33 @@ router.post('/users/sign-up', async (req, res, next) => {
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const [Users, UserAccount] = await prisma.$transaction(async (tx) => {
-            const users = await tx.users.create({
-                data: {
-                    nickname,
-                    password: hashedPassword,
-                    userName,
-                }
-            });
+            try {
+                const users = await tx.users.create({
+                    data: {
+                        nickname,
+                        password: hashedPassword,
+                        userName,
+                    },
+                });
 
-            const userAccount = await tx.users.create({
-                data: {
-                    userId: users.id,
-                    cash: 5000,
-                }
-            });
+                const userAccount = await tx.userAccount.create({
+                    data: {
+                        userId: users.id,
+                        cash: 5000,
+                    },
+                });
 
-            return [users, userAccount]
-
+                return [users, userAccount];
+            } catch (error) {
+                console.error("Transaction Error:", error);
+                // 트랜잭션 실패 시 예외를 다시 던짐
+                throw error;
+            }
         });
+        return res.status(201).json({ Message: `회원가입완료 ${nickname}, ${userName}` })
     } catch (err) {
         next(err);
     }
-    const { nickname, userName } = req.body;
-    return res.status(201).json({ Message: `회원가입완료 ${nickname}, ${userName}` })
 });
 
 // 로그인
@@ -76,11 +81,13 @@ router.post('/users/sign-in', async (req, res, next) => {
 
         if (!user) {
             return res.status(401).json({ Message: '존재하지 않는 아이디입니다.' });
-        } else if (await bcrypt.compare(password, user.password)) {
+        }
+        if (!(await bcrypt.compare(password, user.password))) {
             return res.status(401).json({ Message: '비밀번호가 일치하지 않습니다.' });
         }
-
+        console.log(req.session);
         req.session.nickname = user.nickname;
+       
         const token = jwt.sign(
             {
                 nickname: user.nickname,
@@ -90,17 +97,17 @@ router.post('/users/sign-in', async (req, res, next) => {
                 expiresIn: '10m'
             }
         );
+
+        res.cookie('authMiddleware', `Bearer ${token}`);
+        return res.status(200).json({ Message: `${nickname} 로그인 성공` });
     } catch (err) {
         next(err);
     }
-
-    res.cookie('authMiddleware', `Bearer ${token}`);
-    return res.status(200).json({ Message: `${nickname} 로그인 성공` });
 });
 
 // 캐시 지급
 router.post('/users/cash', authMiddleware, async (req, res, next) => {
-    try{
+    try {
         const { cash } = req.body;
 
         const addCash = await prisma.users.update({
@@ -113,7 +120,7 @@ router.post('/users/cash', authMiddleware, async (req, res, next) => {
             },
         });
         return res.status(201).json({ Message: `${addCash}를 얻었습니다.` });
-    }catch(err){
+    } catch (err) {
         next(err);
     }
 });
