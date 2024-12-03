f8ae8a4397363f0ec7ef3c790facbd3a9e6c8b96
Jaeyoung <jyko1101@gmail.com>
2024-12-03 15:28:53 +0900

    refactor

--- src/utils/gameLogic/gameLogic.js
+++ src/utils/gameLogic/gameLogic.js
@@ -7,8 +7,12 @@
 
 // export 하려는 매치 시뮬레이션 로직
 const simulateMatch = (squad1, squad2) => {
-    let squad1Score = 0;
-    let squad2Score = 0;
+    // 점수 초기화
+    let { squad1Score, squad2Score } = { squad1Score: 0, squad2Score: 0 };
+    // 전처리: 로직계산을 위한 데이터 캐싱
+    const cachedStats = postProcessCachedData(squad1, squad2);
+
+
 
     for (let i = 0; i < 6; i++) {
         const isSecondHalf = i >= 3; // 후반전은 3번째 이후부터
@@ -29,15 +33,12 @@ const simulateMomentum = () => {
 
 };
 
-// 공수 결정
-const determineAtkPos = (squad1, squad2) => {
-    // 공격과 수비의 결정은 양팀의 (패스 + 주력/가속)의 능력치가 결정. (초기 설정계수: 0.65, 0.35)
-    // const sqaud1Param = squad1.pass 
 
-
+// 각각의 모멘텀에서의 계산
+const postProcessCachedData = (squad1, squad2) => {
     const cachedStats = [
         // 스쿼드1 데이터
-        ...userSquad1.map(squad => ({
+        ...squad1.map(squad => ({
             squadId: squad.id,
             players: squad.userteam.players.map(player => ({
                 playerId: player.id,
@@ -50,7 +51,7 @@ const determineAtkPos = (squad1, squad2) => {
         })),
 
         // 스쿼드2 데이터
-        ...userSquad2.map(squad => ({
+        ...squad2.map(squad => ({
             squadId: squad.id,
             players: squad.userteam.players.map(player => ({
                 playerId: player.id,
@@ -62,6 +63,19 @@ const determineAtkPos = (squad1, squad2) => {
             }))
         }))
     ];
+    return cachedStats;
+};
+
+
+
+
+// 공수 결정
+const determineAtkPos = (squad1, squad2) => {
+    // 공격과 수비의 결정은 양팀의 (패스 + 주력/가속)의 능력치가 결정. (초기 설정계수: 0.65, 0.35)
+    // const sqaud1Param = squad1.pass 
+
+
+
 
 }
 
