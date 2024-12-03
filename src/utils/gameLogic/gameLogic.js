0e8cf2456d0960572c8011fef21967982d2241bf
Jaeyoung <jyko1101@gmail.com>
2024-12-03 16:54:11 +0900

    refactor: complete logic

--- src/utils/gameLogic/gameLogic.js
+++ src/utils/gameLogic/gameLogic.js
@@ -4,79 +4,65 @@
 // 게임 로직 함수 구현부
 //====================================================================================================================
 //====================================================================================================================
+import crypto from 'crypto';
 
 // export 하려는 매치 시뮬레이션 로직
 const simulateMatch = (squad1, squad2) => {
     // 점수 초기화
     let { squad1Score, squad2Score } = { squad1Score: 0, squad2Score: 0 };
     // 전처리: 로직계산을 위한 데이터 캐싱
-    const cachedStats = postProcessCachedData(squad1, squad2);
+    const squad1Stats = getCachedStats(squad1);
+    const squad2Stats = getCachedStats(squad2);
+    // 시뮬레이션 4번 반복
+    for (let i = 0; i < 4; i++) {
+        // 각 스쿼드의 플레이어 능력치 합산 계산
+        const squad1TotalStats = calculateTotalStats(squad1Stats);
+        const squad2TotalStats = calculateTotalStats(squad2Stats);
 
-
-
-    for (let i = 0; i < 6; i++) {
-        const isSecondHalf = i >= 3; // 후반전은 3번째 이후부터
-        const momentumResult = simulateMomentum(squad1, squad2, isSecondHalf);
-
-        if (momentumResult === 'Goal!') {
-            team1Score++;
-        } else if (momentumResult === 'Attack Failed') {
-            team2Score++;
+        // 공격권 결정
+        if (getRandOut(squad1TotalStats.paramAtkPos, squad2TotalStats.paramAtkPos)) {
+            squad1Score = simulateSequence(squad1TotalStats.paramAtk, squad2TotalStats.paramDef, squad1Score);
+        } else {
+            squad2Score = simulateSequence(squad2TotalStats.paramAtk, squad1TotalStats.paramDef, squad2Score);
         }
     }
 
     return { squad1Score, squad2Score };
 };
 
-// 각각의 모멘텀에서의 계산
-const simulateMomentum = () => {
-
-};
-
 
 // 각각의 모멘텀에서의 계산
-const postProcessCachedData = (squad1, squad2) => {
-    const cachedStats = [
-        // 스쿼드1 데이터
-        ...squad1.map(squad => ({
-            squadId: squad.id,
-            players: squad.userteam.players.map(player => ({
-                playerId: player.id,
-                stamina: player.playerStats.stamina,
-                // 전처리 계산값 캐싱: 공격권 결정 변수, 공격 변수, 수비 변수
-                paramAtkPos: 0.65 * player.playerStats.pass + 0.35 * player.playerStats.pace,
-                paramAtk: 0.5 * player.playerStats.finishing + 0.2 * player.playerStats.technique + 0.3 * player.playerStats.agility,
-                paramDef: 0.65 * player.playerStats.defense + 0.35 * player.playerStats.agility,
-            }))
-        })),
+const getCachedStats = (squad) => squad.userteam.players.map(player => ({
+    stamina: player.playerStats.stamina,
+    // 전처리 계산값 캐싱: 공격권 결정 변수, 공격 변수, 수비 변수
+    paramAtkPos: 0.65 * player.playerStats.pass + 0.35 * player.playerStats.pace,
+    paramAtk: 0.5 * player.playerStats.finishing + 0.2 * player.playerStats.technique + 0.3 * player.playerStats.agility,
+    paramDef: 0.65 * player.playerStats.defense + 0.35 * player.playerStats.agility,
+}));
 
-        // 스쿼드2 데이터
-        ...squad2.map(squad => ({
-            squadId: squad.id,
-            players: squad.userteam.players.map(player => ({
-                playerId: player.id,
-                stamina: player.playerStats.stamina,
-                // 전처리 계산값 캐싱: 공격권 결정 변수, 공격 변수, 수비 변수
-                paramAtkPos: 0.65 * player.playerStats.pass + 0.35 * player.playerStats.pace,
-                paramAtk: 0.5 * player.playerStats.finishing + 0.2 * player.playerStats.technique + 0.3 * player.playerStats.agility,
-                paramDef: 0.65 * player.playerStats.defense + 0.35 * player.playerStats.agility,
-            }))
-        }))
-    ];
-    return cachedStats;
+
+// x, y 인자를 받고 랜덤하게 확률 결과
+const getRandOut = (x, y) => {
+    const rand = crypto.randomInt(0, x + y);
+    return (rand <= x);
 };
 
-
-
-
-// 공수 결정
-const determineAtkPos = (squad1, squad2) => {
-    // 공격과 수비의 결정은 양팀의 (패스 + 주력/가속)의 능력치가 결정. (초기 설정계수: 0.65, 0.35)
-    // const sqaud1Param = squad1.pass 
-
-
-
-
-}
+// 공격수비 상황 로직
+const simulateSequence = (atk, def, score) => {
+    if (getRandOut(atk, def)) score++;
+    return score;
+};
+// 리듀서 사용해서
+const calculateTotalStats = (players) => {
+    return players.reduce((total, player) => {
+        total.paramAtkPos += player.paramAtkPos * (player.stamina / 100);
+        total.paramAtk += player.paramAtk * (player.stamina / 100);
+        total.paramDef += player.paramDef * (player.stamina / 100);
+        // 스테미너 10% 감소
+        player.stamina *= 0.9;
+        return total;
+    }, 
+    { paramAtkPos: 0, paramAtk: 0, paramDef: 0 });
+};
 
 export default simulateMatch;
