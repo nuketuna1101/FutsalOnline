5269933916115cd9f061d837ba61a1bd822632be
leejunsung <junstar96@naver.com>
2024-12-04 16:57:31 +0900

    staged

--- src/routes/transfermarket.router.js
+++ src/routes/transfermarket.router.js
@@ -4,6 +4,33 @@ import authMiddleware from '../middlewares/auth.middleware.js'
 
 const router = express.Router();
 
+//이적 시장 등록
 router.post('/transfermarket', authMiddleware, (req,res,next)=>{
+    const {userTeamId} = req.userTeamId;
 
+
+
+})
+
+//이적 시장 구매
+router.post('/transfermarket', authMiddleware, (req,res,next)=>{
+    const {userTeamId} = req.userTeamId;
+
+})
+
+
+//매물 목록 조회
+router.get('/transfermarket', authMiddleware, (req,res,next)=>{
+    
+})
+
+//매물 검색 조회
+router.get('/transfermarket/:playerId', authMiddleware, (req,res,next)=>{
+    
+})
+
+//등록된 매물 등록 취소
+router.delete('/transfermarket', authMiddleware, (req,res,next)=>{
+    const {playerId} = req.playerId;
+    //취소된 것은 그냥 바로 teams에 들어오면 되는 거 아닌가.
 })
